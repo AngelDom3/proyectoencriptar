f41552c7d22494067d561859c705288732f0adb6
Angel Dominguez <angeladom777@gmail.com>
2023-05-27 02:02:53 -0500

    encriptador y desencriptador funcionales

--- proyectoj.js
+++ proyectoj.js
@@ -1,15 +1,32 @@
+/*encriptador */
 var botonEncriptar = document.querySelector(".boton-encriptador")
 botonEncriptar.onclick = encriptador;
 
-
 function encriptador(){
     var textoPrincipal = document.getElementById("texto-principal").value;
-     
-     textoPrincipal= textoPrincipal.replace (/e/g,"enter");
-     textoPrincipal= textoPrincipal.replace (/i/g,"imes");
-     textoPrincipal= textoPrincipal.replace (/a/g,"ai");
-     textoPrincipal= textoPrincipal.replace (/o/g,"ober");
-     textoPrincipal= textoPrincipal.replace (/u/g,"ufat");
+         textoPrincipal= textoPrincipal.replace (/e/g,"enter");
+         textoPrincipal= textoPrincipal.replace (/i/g,"imes");
+         textoPrincipal= textoPrincipal.replace (/a/g,"ai");
+         textoPrincipal= textoPrincipal.replace (/o/g,"ober");
+         textoPrincipal= textoPrincipal.replace (/u/g,"ufat");
     document.getElementById("texto-apartado").value=textoPrincipal;
+}/*encriptador */
+
+ /*desencriptador */
+var botondesencriptar = document.querySelector(".boton-desencriptador")
+botondesencriptar.onclick = desencriptar;
+
+function desencriptar(){
+var textoPrincipal = document.getElementById("texto-principal").value;
+
+    textoPrincipal= textoPrincipal.replace (/enter/g,"e");
+    textoPrincipal= textoPrincipal.replace (/imes/g,"i");
+    textoPrincipal= textoPrincipal.replace (/ai/g,"a");
+    textoPrincipal= textoPrincipal.replace (/ober/g,"o");
+    textoPrincipal= textoPrincipal.replace (/ufat/g,"u");
+document.getElementById("texto-apartado").value=textoPrincipal;
+
+
 
 }
+ /*desencriptador */

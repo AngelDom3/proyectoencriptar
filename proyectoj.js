c3a01132549e9bd95874edf62625e3dd0d12b03f
Angel Dominguez <angeladom777@gmail.com>
2023-05-27 00:42:33 -0500

    nada

---
+++ proyectoj.js
@@ -0,0 +1,19 @@
+
+    
+var hola = document.querySelector(".boton-encriptador");
+  hola.onclick = verificar;
+
+function verificar(){
+
+    alert("hola word")
+}
+
+var adios = document.querySelector(".boton-desencriptador");
+  adios.onclick = desverificar;
+
+function desverificar(){
+
+    alert("adios word")
+}
+ var textoApartado = document.querySelector(".texto-apartado")
+ 

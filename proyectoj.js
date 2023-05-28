d5b306a4caedf3aaeaa95c5878dbe6ae44cf6b5d
Angel Dominguez <angeladom777@gmail.com>
2023-05-28 00:09:52 -0500

    funcionalidad del boton copiar y eliminar

--- proyectoj.js
+++ proyectoj.js
@@ -12,7 +12,7 @@ function encriptador(){
     document.getElementById("texto-apartado").value=textoPrincipal;
 }/*fin De Encriptador */
 
- /*Esencriptador */
+ /*Desencriptador */
 var botondesencriptar = document.querySelector(".boton-desencriptador")
 botondesencriptar.onclick = desencriptar;
 
@@ -26,4 +26,27 @@ var textoPrincipal = document.getElementById("texto-principal").value;
     textoPrincipal= textoPrincipal.replace (/ufat/g,"u");
 document.getElementById("texto-apartado").value=textoPrincipal;
 }
- /*Esencriptador */
+ /*Fin de Desencriptador */
+
+ /*copiar texto y eliminarlo*/
+ var botonCopiar = document.querySelector(".boton-copiar")
+     botonCopiar.onclick = copiarYEliminar;
+
+ function copiarYEliminar(){
+    
+    
+     var textoCopiar = document.querySelector(".texto-apartado")
+
+     textoCopiar.select()
+     document.execCommand("copy")
+     document.execCommand("delete")
+
+     
+     var principalEliminar = document.querySelector(".texto-principal")
+     principalEliminar.select()
+     document.execCommand("delete")
+
+
+ }
+
+ /*copiar texto y eliminarlo*/
